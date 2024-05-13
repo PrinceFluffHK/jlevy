85300e898355923cdf329aff977afcb05fa2ba30
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2024-05-13 18:19:33 -0400

    oops fixed it

--- src/data/shortFilms.js
+++ src/data/shortFilms.js
@@ -4,19 +4,22 @@ export const oneMansTrash = new ShortFilm(
     "One Man's Trash",
     "Director, Screenwriter, and Editor",
     "A homeless man must defend his territory when an intruder arrives at the dump.",
-    "wE9Ryd02b0I"
+    "wE9Ryd02b0I",
+    []
 );
 
 export const boomerangBall = new ShortFilm(
     "Boomerang Ball",
     "Director, Screenwriter, and Editor",
     "A woman struggles to rid herself of a persistent volley ball.",
-    "tNQF4moZi6I"
+    "tNQF4moZi6I",
+    []
 );
 
 export const setupToSmash = new ShortFilm(
     "Setup to Smash",
     "Director, Screenwriter, and Editor",
     "Two friends setup the equipment needed for a video game club.",
-    "8lfTrFGczFA"
+    "8lfTrFGczFA",
+    []
 );
